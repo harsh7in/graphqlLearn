2057bcd5f38bddaa0fa482d2ecb2fb79e36348b8
harsh1dev <harshkumar1.dev@gmail.com>
2024-09-05 16:55:23 +0530

    graphql data coming

--- server/index.js
+++ server/index.js
@@ -1,33 +1,64 @@
 const express = require('express');
-const {ApolloServer}  = require('@apollo/server');
-const { expressMiddleware} = require('@apollo/server/express4');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const { TODOS } = require('./todo');
+const { USER } = require('./user');
 
+async function startWorking() {
 
-
-async function startWorking(){
-
+    // console.log(USER)
     const app = express();
-    const server = new ApolloServer({
-        typeDefs: `
+
+    const typeDefs = `
+            type Todo {
+                id: ID!
+                title: String!
+                completed: Boolean
+                user:User
+            }
+            
+            type User {
+                id: ID!
+                name:String!
+                website:String!
+                email: String!
+                username:String!
+            }
+
             type Query {
                 hello: String
+                getTodos: [Todo]
+                getAllUsers: [User]
             }
-        `,
-        resolvers: {
-            Query: {
-                hello: () => 'Hello World!',
-            },
+        `
+    const resolvers = {
+        Todo: {
+            user: (todo) => USER.find((e) => e.id === todo.id),
         },
+        Query: {
+            hello: () => 'Hello World!',
+            getTodos: () => TODOS,
+            getAllUsers: () => USER
+        },
+    }
+
+
+
+
+    const server = new ApolloServer({
+        typeDefs: typeDefs,
+        resolvers: resolvers
     });
+
 
     app.use(bodyParser.json());
     app.use(cors());
 
     await server.start();
 
-    app.use("/graphql",expressMiddleware(server));
+    app.use("/graphql", expressMiddleware(server));
 
 
     app.listen(3000, () =>
